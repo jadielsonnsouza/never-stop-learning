07f8261ab2c923cfbcef2e50e6f6f8fd3e9e6b03
jadielsonnsouza <jadielsonnsouza@gmail.com>
2021-06-15 21:45:39 -0300

    Criado o projeto calculadora de signos

---
+++ C#/MinhaCalculadoraDeSignos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MinhaCalculadoraDeSignos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Signo signo = new Signo();
+            signo.nome = "Libra";
+
+            Console.WriteLine("Seu signo é : " + signo.nome);
+        }
+    }
+}
